3b661c4dca0d2e7670b69e2f48f0fc60bfb5b140
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2024-12-28 17:06:32 -0300

    Consertar links da homepage

--- view/home/homepage.php
+++ view/home/homepage.php
@@ -14,32 +14,32 @@
         <ul>
             <li>Clientes
                 <ul>
-                    <li><a href="../../index.php?classe=Cliente&action=index">Listar Todos</a></li>
-                    <li><a href="../../index.php?classe=Cliente&action=create">Novo Cliente</a></li>
+                    <li><a href="../../index.php?classe=Cliente&metodo=index">Listar Todos</a></li>
+                    <li><a href="../../index.php?classe=Cliente&metodo=create">Novo Cliente</a></li>
                 </ul>
             </li>
             <li>Serviços
                 <ul>
-                    <li><a href="../../index.php?classe=Servico&action=index">Listar Todos</a></li>
-                    <li><a href="../../index.php?classe=Servico&action=create">Novo Serviço</a></li>
+                    <li><a href="../../index.php?classe=Servico&metodo=index">Listar Todos</a></li>
+                    <li><a href="../../index.php?classe=Servico&metodo=create">Novo Serviço</a></li>
                 </ul>
             </li>
             <li>Produtos
                 <ul>
-                    <li><a href="../../index.php?classe=Produto&action=index">Listar Todos</a></li>
-                    <li><a href="../../index.php?classe=Produto&action=create">Novo Produto</a></li>
+                    <li><a href="../../index.php?classe=Produto&metodo=index">Listar Todos</a></li>
+                    <li><a href="../../index.php?classe=Produto&metodo=create">Novo Produto</a></li>
                 </ul>
             </li>
             <li>Agendamentos
                 <ul>
-                    <li><a href="../../index.php?classe=Agendamento&action=index">Listar Todos</a></li>
-                    <li><a href="../../index.php?classe=Agendamento&action=create">Novo Agendamento</a></li>
+                    <li><a href="../../index.php?classe=Agendamento&metodo=index">Listar Todos</a></li>
+                    <li><a href="../../index.php?classe=Agendamento&metodo=create">Novo Agendamento</a></li>
                 </ul>
             </li>
             <li>Compras
                 <ul>
-                    <li><a href="../../index.php?classe=Cliente&metodo=index">Listar Todas</a></li>
-                    <li><a href="../../index.php?classe=Compra&action=create">Nova Compra</a></li>
+                    <li><a href="../../index.php?classe=Compra&metodo=index">Listar Todas</a></li>
+                    <li><a href="../../index.php?classe=Compra&metodo=create">Nova Compra</a></li>
                 </ul>
             </li>
         </ul>
